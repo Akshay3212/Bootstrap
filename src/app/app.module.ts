31f459e88333b4fc42da087254a9ebc239517321
Akshay <akshbabar3212@gmail.comgit config --global user.email akshbabar3212@gmail.comgit config --global user.email akshbabar3212@gmail.com>
2024-06-02 09:48:42 +0530

    Done by Akshay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditComponent } from './edit/edit.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent,
+    EditComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
